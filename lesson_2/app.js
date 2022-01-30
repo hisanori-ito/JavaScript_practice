30c736d87f3cf8edca55f8ca3cd7f1366e8633c3
伊藤久剛 <itohisanori@itohisanorinoMacBook-Pro.local>
2022-01-30 18:09:20 +0900

    [Finish]chapter2

---
+++ lesson_2/app.js
@@ -0,0 +1,62 @@
+// let hello = "Hello World";
+// alert(hello);
+
+// let init1 = 1;
+
+// let init2 = -10;
+
+// let float1 = 3.14;
+
+// let str1 = 'JavaScriptを覚えよう';
+
+// alert(init1 + init2);
+// alert(8 - 5);
+
+// let orange = 100;
+// let apple = 120;
+
+// if(orange < apple){
+//   alert("みかんの値段がりんごより安い");
+// } else if(orange == apple){
+//   alert(みかんとりんごが同じ値段);
+// } else {
+//   alert('みかんの値段がりんごより安い');
+// }
+
+// let max = 1000;
+// let num = 1;
+// let count = 0;
+
+// while (num < max){
+//   num *= 2;
+//   count += 1;
+// }
+
+// alert('２をかけて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// do {
+//   alert(hello);
+// }while(num < max){
+//   num *= 2;
+//   count += 1;
+// }
+
+// alert('２をかけて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// let i;
+// let num = 0;
+
+// for(i = 1; i < 11; i++){
+//   num += i;
+// }
+
+// alert('1から10まで足し算した結果は' + num + 'です');
+
+let int1 = 3;
+let int2 = 8;
+let float1 = 2.2;
+
+alert(int1 + int2 + float1);
+alert(int1 - int2 - float1);
+alert(int1 * int2 * float1);
+alert(int1 / int2 / float1);
